7e3e5be1c771d0410af9d7313e32e3ced04bb983
Rasmus Villemoes <rv@rasmusvillemoes.dk>
2015-06-25 23:44:18 +0200

    debug: print some time stamps from create_db.sh
    To make it slightly easier to measure the effect of changes to various
    scripts, sprinkle a few time stamp prints in create_db.sh.
    
    Signed-off-by: Rasmus Villemoes <rv@rasmusvillemoes.dk>

--- smatch_data/db/create_db.sh
+++ smatch_data/db/create_db.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+stamp() {
+    now=$(date '+%F %T')
+    echo "$now $1"
+}
+
 if echo $1 | grep -q '^-p' ; then
     PROJ=$(echo $1 | cut -d = -f 2)
     shift
@@ -21,20 +26,31 @@ for i in ${bin_dir}/*.schema ; do
     cat $i | sqlite3 $db_file
 done
 
+stamp fill_db_sql.sh
 ${bin_dir}/fill_db_sql.sh $info_file $db_file
+stamp fill_db_caller_info.pl
 ${bin_dir}/fill_db_caller_info.pl "$PROJ" $info_file $db_file
+stamp build_early_index.sh
 ${bin_dir}/build_early_index.sh $db_file
 
+stamp fill_db_type_value.pl
 ${bin_dir}/fill_db_type_value.pl "$PROJ" $info_file $db_file
+stamp fill_db_type_size.pl
 ${bin_dir}/fill_db_type_size.pl "$PROJ" $info_file $db_file
+stamp build_late_index.sh
 ${bin_dir}/build_late_index.sh $db_file
 
+stamp fixup_all.sh
 ${bin_dir}/fixup_all.sh $db_file
 if [ "$PROJ" != "" ] ; then
     ${bin_dir}/fixup_${PROJ}.sh $db_file
 fi
 
+stamp remove_mixed_up_pointer_params.pl
 ${bin_dir}/remove_mixed_up_pointer_params.pl $db_file
+stamp mark_function_ptrs_searchable.pl
 ${bin_dir}/mark_function_ptrs_searchable.pl $db_file
 
 mv $db_file smatch_db.sqlite
+
+stamp "All done"
